46c9acb330fa8c8cde7afe7f4b232d276837b29b
mstuarthitachi <85649082+mstuarthitachi@users.noreply.github.com>
2024-01-12 14:49:29 -0800

    Update deploy.py

--- example_ml_project/notebooks/ci/deploy.py
+++ example_ml_project/notebooks/ci/deploy.py
@@ -1,5 +1,7 @@
 import argparse
 
+import api_client
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--client-id", required=True)
 parser.add_argument("--client-secret", required=True)
@@ -9,8 +11,8 @@ parser.add_argument("--environment", required=True)
 args = parser.parse_args()
 
 # argparse changes dashes to underscores
-# print(f"The value of client-id is {args.client_id}")
-# print(f"The value of client-secret is {args.client_secret}")
-# print(f"The value of tenant-id is {args.tenant_id}")
-# print(f"The value of databricks-url is {args.databricks_url}")
-# print(f"The value of environment is {args.environment}")
+print(f"The value of client-id is {args.client_id}")
+print(f"The value of client-secret is {args.client_secret}")
+print(f"The value of tenant-id is {args.tenant_id}")
+print(f"The value of databricks-url is {args.databricks_url}")
+print(f"The value of environment is {args.environment}")
