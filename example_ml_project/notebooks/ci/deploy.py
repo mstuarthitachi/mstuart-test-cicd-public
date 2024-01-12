6ca48bd10277248bc36e561d783127217047d856
mstuarthitachi <85649082+mstuarthitachi@users.noreply.github.com>
2024-01-12 14:05:21 -0800

    Update deploy.py

--- example_ml_project/notebooks/ci/deploy.py
+++ example_ml_project/notebooks/ci/deploy.py
@@ -8,8 +8,11 @@ parser.add_argument("--databricks-url", required=True)
 parser.add_argument("--environment", required=True)
 args = parser.parse_args()
 
-print(f"The value of client-id is {args.client-id}")
-print(f"The value of client-secret is {args.client-secret}")
-print(f"The value of tenant-id is {args.tenant-id}")
-print(f"The value of databricks-url is {args.databricks-url}")
-print(f"The value of environment is {args.environment}")
+client_id_var = args.client-id
+print(f"The value of client-id is {client_id_var}")
+
+# print(f"The value of client-id is {args.client-id}")
+# print(f"The value of client-secret is {args.client-secret}")
+# print(f"The value of tenant-id is {args.tenant-id}")
+# print(f"The value of databricks-url is {args.databricks-url}")
+# print(f"The value of environment is {args.environment}")
